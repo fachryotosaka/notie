a2464e763d593e08fbec1790155944107b9f57dd
fachryotosaka <fachry2710@gmail.com>
2024-04-25 16:21:03 +0700

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Greates Woman Dwina Andita ( Na's )
+        My Greates Woman Dwina Andita 
       </h1>
       <p className="mb-4">
-        {`Na's, my incredible princess, radiates pure wonder and beauty in every way imaginable. She's so perfect in various aspects, and I love her wholeheartedly. I'm grateful and incredibly proud to have met her :) .`}
+        {`Dwina andita ( Na's ), my incredible princess, radiates pure wonder and beauty in every way imaginable. She's so perfect in various aspects, and I love her wholeheartedly. I'm grateful and incredibly proud to have met her :) .`}
       </p>
       <div className="my-8">
         <BlogPosts />
